6c37257a370f9a6a7d26225f0fc16b5ff1e2353d
Nimesha <rdnw005@gmail.com>
2025-05-28 00:19:22 +0530

    fully working final commit

--- lern/my.py
+++ lern/my.py
@@ -1,17 +1,43 @@
-mas_pwd = input("Enter the passwerd to edit or viev the file:")
 from cryptography.fernet import Fernet
 
+
+def load_key():
+     file = open("key.key", "rb")
+     key = file.read()
+     file.close()
+     return key
+
+mas_pwd = input("Enter the passwerd to edit or viev the file:")
+key = load_key() + mas_pwd.encode()
+fer = Fernet(key)
+
+# '''
+# def generate_key():
+#     key = Fernet.generate_key()
+#     with open("key.key", "wb") as key_file:
+#          key_file.write(key)
+# '''
+
+# generate_key()
+
+
+
+
 def add():
-      name = input("account name: ")
-      pwd = input("account password: ")
-       
-      with open('passwords.txt','a') as f:
-            f.write(name + "|" + pwd + "\n")
+    name = input("account name: ")
+    pwd = input("account password: ")
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+
+
+
 def view():
       with open('passwords.txt','r') as f:
-            for line in f:
-                name, pwd = line.strip().split('|')
-                print(f"Account: {name}, Password: {pwd}")
+            for line in f.readlines():
+                deta = line.rstrip()
+                name, pwd = deta.split("|")
+                print("User", name, "| has password", fer.decrypt(pwd.encode()).decode())
+                
 while mas_pwd == "1234":
      print("You can edit or view the file now.")
      ask = input("do you want to add or view the file? (add/view) or type q to quit: ").lower()
