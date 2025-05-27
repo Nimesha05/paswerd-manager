0f134299f6b45307203c8df52a1e91568f9c0b41
Nimesha <rdnw005@gmail.com>
2025-05-27 20:42:04 +0530

    add view option and new psw

--- lern/my.py
+++ lern/my.py
@@ -1,4 +1,5 @@
 mas_pwd = input("Enter the passwerd to edit or viev the file:")
+from cryptography.fernet import Fernet
 
 def add():
       name = input("account name: ")
@@ -7,10 +8,13 @@ def add():
       with open('passwords.txt','a') as f:
             f.write(name + "|" + pwd + "\n")
 def view():
-    pass
+      with open('passwords.txt','r') as f:
+            for line in f:
+                name, pwd = line.strip().split('|')
+                print(f"Account: {name}, Password: {pwd}")
 while mas_pwd == "1234":
      print("You can edit or view the file now.")
-     ask = input("do you want to add or view the file? (add/viev) or type q to quit: ").lower()
+     ask = input("do you want to add or view the file? (add/view) or type q to quit: ").lower()
      if ask == "q":
         break 
      elif ask == "add":
